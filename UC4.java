ee271fefa46fd8aeae07cbee78be68630893d23d
dhruv121988 <sarkardhruv1219@gmail.com>
2026-03-09 15:07:31 +0530

    Add files via upload

---
+++ UC4.java
@@ -0,0 +1,33 @@
+public class UC4 {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "level";
+
+        // Convert string to character array
+        char[] characters = word.toCharArray();
+
+        // Two-pointer technique
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
+}
